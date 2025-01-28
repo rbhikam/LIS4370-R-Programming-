fc6e57969c721f3ce67c51829ac6fdc7fd01eed1
rbhikam <rbhikam07@yahoo.com>
2025-01-28 09:30:26 -0500

    Add files via upload

---
+++ Module#3 Assignment.R
@@ -0,0 +1,6 @@
+Name<-c("Jeb","Donald","Ted","Marco","Carly","Hillary","Bernie")
+ABCpollresults<-c(4, 62, 51, 21, 2, 14, 15)
+CBSpollresults<-c(12, 75, 43, 19, 1, 21, 19)
+PollResults<-data.frame(Name,ABCpollresults,CBSpollresults)
+PollResults
+
